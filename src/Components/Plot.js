c310684f8793f75cdeaed51598556636d1992951
Arunima-ScaleOrange <125435625+Arunima-ScaleOrange@users.noreply.github.com>
2023-03-23 20:06:53 +0530

    Update Plot.js

--- src/Components/Plot.js
+++ src/Components/Plot.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Plot.css';
-// import plot1 from '../Assets/plot-img-1.svg';
-import Nimmavaram from '../Assets/Nimmavaram';
+import Trial from '../Assets/Trial.js';
+// import Nimmavaram from '../Assets/Nimmavaram';
 
 const Plot = () => {
   return (
     <div className='container'>
     {/* <img src={plot1} alt="plot" className='plot-img' /> */}
-    <Nimmavaram />
+    <Trial />
     </div>
   )
 }
